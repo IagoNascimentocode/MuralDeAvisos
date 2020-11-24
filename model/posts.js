6dacfd09aa8ed6c318fe32aaf0a7ce26ca15438b
iago_ns@hotmail.com <iago_ns@hotmail.com>
2020-11-24 15:00:35 -0300

    MuralDeAvisos

---
+++ model/posts.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+    posts: [        
+        {
+            id:"asjbajsbc",
+            title:"Teste do mural",
+            description:"Descrição de teste"
+        },
+    ],
+        
+        getAll(){
+
+            return this.posts;
+        },
+
+        newPost(title,description){
+
+            this.posts.push({id:generateID(),title,description});
+        }
+
+        
+    }
+        function generateID(){
+      
+            return Math.random().toString(36).substr(2,9);
+            }
